30f7ffdc013730e8ced652e58f67ea35b70c2c43
ritikesh-gokhe <ritikesh.m.gokhe@gmail.com>
2022-11-06 20:11:59 +0530

    I added Tip calculator and Rock Paper Sieser Projects

---
+++ Rock Paper Sieser/script.js
@@ -0,0 +1,108 @@
+/*
+  Rock Paper Scissors 🚀🔥
+  Concepts covered in this project
+    👉 For loops
+    👉 Dom Manipulation
+    👉 Variables
+    👉 Conditionals (if else if)
+    👉 Template Literals
+    👉 Event Listeners
+    👉 Higher order Function (Math.random())
+*/
+
+// ** getComputerChoice randomly selects between `rock` `paper` `scissors` and returns that string **
+// getComputerChoice() 👉 'Rock'
+// getComputerChoice() 👉 'Scissors'
+function getComputerChoice() {
+  const arr = ['Rock', 'Paper', 'Scissors']
+  const res = arr[Math.floor(Math.random() * arr.length)]
+  console.log(res)
+  return res
+}
+// ** getResult compares playerChoice & computerChoice and returns the score accordingly **
+// human wins - getResult('Rock', 'Scissors') 👉 1
+// human loses - getResult('Scissors', 'Rock') 👉 -1
+// human draws - getResult('Rock', 'Rock') 👉 0
+function getResult(playerChoice, computerChoice) {
+  let score
+  if (playerChoice == computerChoice) {
+    score = 0
+  } else if (
+    (playerChoice == 'Rock' && computerChoice == 'Scissors') ||
+    (playerChoice == 'Scissors' && computerChoice == 'Paper') ||
+    (playerChoice == 'Paper' && computerChoice == 'Rock')
+  ) {
+    score = 1
+  } else {
+    score = -1
+  }
+  return score
+
+  // return the result of score based on if you won, drew, or lost
+  // All situations where human draws, set `score` to 0
+  // All situations where human wins, set `score` to 1
+  // make sure to use else ifs here
+  // Otherwise human loses (aka set score to -1)
+  // return score
+}
+
+// ** showResult updates the DOM to `You Win!` or `You Lose!` or `It's a Draw!` based on the score. Also shows Player Choice vs. Computer Choice**
+function showResult(score, playerChoice, computerChoice) {
+  const result = document.getElementById('result')
+  if (score == -1) {
+    result.innerText = 'You Lose'
+  } else if (score == 1) {
+    result.innerText = 'You Win'
+  } else {
+    result.innerText = "It's a Draw!"
+  }
+
+  let playerScore = document.getElementById('player-score')
+  let hand = document.getElementById('hands')
+  playerScore.innerText = `${Number(playerScore.innerText) + score}`
+  hands.innerText = `👱 ${playerChoice} vs 🤖 ${computerChoice}`
+
+  // Hint: on a score of -1
+  // You should do result.innerText = 'You Lose!'
+  // Don't forget to grab the div with the 'result' id!
+}
+
+// ** Calculate who won and show it on the screen **
+function onClickRPS(playerChoice) {
+  const computerChoice = getComputerChoice()
+  const score = getResult(playerChoice.value, computerChoice)
+  showResult(score, playerChoice.value, computerChoice)
+}
+
+// ** Make the RPS buttons actively listen for a click and do something once a click is detected **
+function playGame() {
+  // use querySelector to select all RPS Buttons
+  let rpsButtons = document.querySelectorAll('.rpsButton')
+
+  // * Adds an on click event listener to each RPS button and every time you click it, it calls the onClickRPS function with the RPS button that was last clicked *
+
+  // 1. loop through the buttons using a forEach loop
+  // 2. Add a 'click' event listener to each button
+  // 3. Call the onClickRPS function every time someone clicks
+  // 4. Make sure to pass the currently selected rps button as an argument
+
+  rpsButtons.forEach((rpsButton) => {
+    rpsButton.onclick = () => onClickRPS(rpsButton)
+  })
+
+  // Add a click listener to the end game button that runs the endGame() function on click
+  let endGameButton = document.getElementById('endGameButton')
+  endGameButton.onclick = () => endGame()
+}
+
+// ** endGame function clears all the text on the DOM **
+function endGame() {
+  let playerScore = document.getElementById('player-score')
+  let hands = document.getElementById('hands')
+  let result = document.getElementById('result')
+  playerScore.innerText = ''
+  hands.innerText = ''
+  result.innerText = ''
+}
+
+playGame()
